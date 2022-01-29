439263958d094b70f1747b045714f621490c1a58
tolanie <florenceabiodun2000@yahoo.com>
2022-01-29 08:39:02 +0100

    concert Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import './Apps.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="concert">
+      <div className= 'main'>
+        <div className='card'>
+
+        <div className='show'>
+        <h1>LifeStyle Party</h1>
+        <p>Book your ticket with ease</p>
+        </div>
+        
+        <div className='showcase'>
+          <form>
+            <div className='group'>
+            <label>First Name</label>
+            <input type='name' for='name' placeholder='Enter First Name'></input>
+            </div>
+
+            <div className='group'>
+            <label>Last Name</label>
+            <input type='name' for='name' placeholder='Enter Last Name'></input>
+            </div>
+
+            <div className='group'> 
+            <label>Email</label>
+            <input type='email' for='email' placeholder='Enter email'></input>
+            </div>
+          </form>
+          <button>Click here to book</button>
+        </div>
+        </div>
+
+      </div>
     </div>
   );
 }

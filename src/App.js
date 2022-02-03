f1bbf81f74773b13bccbbffb968488f3acaf948c
tolanie <florenceabiodun2000@yahoo.com>
2022-02-03 12:20:53 +0100

    complete front end for Lifestyle concert

--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { closePaymentModal, useFlutterwave } from "flutterwave-react-v3"
 
 import './Apps.scss'
 
 function App() {
+  const [{ firstName, lastName, email }, setFields] = useState({
+    email: "",
+    firstName: "",
+    lastName: ""
+  })
+
+  const config = {
+    public_key: "",
+    amount: 10,
+    currency: "NGN",
+    customer: {
+      email,
+      name: `${firstName} ${lastName}`,
+      phonenumber: "09088765433"
+    },
+    customization: {
+      title: "Lifestyle Party",
+      description: "Pay now, to register for turnah's concert"
+    },
+    tx_ref: Date.now()
+  }
+
+  const handleInput = (e) => {
+    setFields(prevState => ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    handleFlutter({
+      callback: (res) => {
+        console.log(res);
+        closePaymentModal();
+      }
+    })
+  }
+
+  const handleFlutter = useFlutterwave(config)
+
   return (
     
     <div className="concert">
       <div className= 'main'>
-        <div className='card'>
+        <div className='card' >
 
         <div className='show'>
         <h1>LifeStyle Party</h1>
@@ -15,23 +57,23 @@ function App() {
         </div>
         
         <div className='showcase'>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className='group'>
-            <label>First Name</label>
-            <input type='name' for='name' placeholder='Enter First Name'></input>
+            <label for="firstName">First Name</label>
+            <input type='text' value={firstName}  id='firstName' name="firstName" placeholder='Enter First Name' required="true" onChange={handleInput} />
             </div>
 
             <div className='group'>
-            <label>Last Name</label>
-            <input type='name' for='name' placeholder='Enter Last Name'></input>
+            <label for="lastName">Last Name</label>
+            <input type='text' value={lastName} id="lastName" name="lastName" placeholder='Enter Last Name' required="true" onChange={handleInput} />
             </div>
 
             <div className='group'> 
-            <label>Email</label>
-            <input type='email' for='email' placeholder='Enter email'></input>
+            <label for="email">Email</label>
+            <input type='email' id="email" value={email} name="email" placeholder='Enter email' required="true" onChange={handleInput} />
             </div>
+            <button type="submit"><p>Click here to book</p></button>
           </form>
-          <button>Click here to book</button>
         </div>
         </div>
 

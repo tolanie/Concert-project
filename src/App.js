8d3538526e62210406d7c02f9926023e8d5efd51
DanLowo <danlowo@gmail.com>
2022-02-15 10:51:23 +0100

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { closePaymentModal, useFlutterwave } from "flutterwave-react-v3";
 
 import "./Apps.scss";
+import SuccessImg from "./img/success.png";
+import FailImg from "./img/fail.png";
 
 function App() {
   const [{ fullName, email }, setFields] = useState({
@@ -10,16 +12,57 @@ function App() {
   });
 
   const [price, setPrice] = useState(0);
-  const [isPaymentSelected, setIsPaymentSelected] = useState(false)
+  const [isPaymentSelected, setIsPaymentSelected] = useState(false);
+  const [showStatus, setShowStatus] = useState(false);
+  const [paymentDetails, setPaymentDetails] = useState({
+    amount: 2000,
+    currency: "NGN",
+    status: "successful",
+    name: "Hello World",
+  });
 
   const paymentOption = [
-    { type: "Regular", price: "2000" },
-    { type: "VIP", price: "5000" },
-    // { type: "VVIP", price: "20000" },
+    { type: "Regular", price: 10 },
+    { type: "VIP", price: 5000 },
   ];
 
+  const handleInput = (e) => {
+    setFields((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (price === 0) {
+      setIsPaymentSelected(true);
+      return;
+    }
+    setIsPaymentSelected(false);
+    handleFlutter({
+      callback: (res) => {
+        console.log(res);
+        closePaymentModal();
+        setPaymentDetails(res);
+        setShowStatus(true);
+      },
+    });
+  };
+
+  const handleClose = () => {
+    if (showStatus) {
+      setFields({
+        email: "",
+        fullName: "",
+      });
+      setPrice(0)
+    }
+    setShowStatus(false);
+  };
+
   const config = {
-    public_key: "",
+    public_key: "FLWPUBK-6c22bb36e48a919db211777df257a762-X", //"FLWPUBK_TEST-94cc9257f32ea519c9f2012d9c7290c7-X",
     amount: price,
     currency: "NGN",
     customer: {
@@ -33,95 +76,102 @@ function App() {
     },
     tx_ref: Date.now(),
   };
+  const handleFlutter = useFlutterwave(config);
 
-  const handleInput = (e) => {
-    setFields((prevState) => ({
-      ...prevState,
-      [e.target.name]: e.target.value,
-    }));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if(price === 0) {
-      setIsPaymentSelected(true)
-      return;
-    }
-    setIsPaymentSelected(false)
-    // handleFlutter({
-    //   callback: (res) => {
-    //     console.log(res);
-    //     closePaymentModal();
-    //   }
-    // })
-  };
-
-  // const handleFlutter = useFlutterwave(config)
+  const PaymentStatus = ({ details }) => (
+    <div className="paymentCard">
+      {details?.status === "successful" ? (
+        <>
+          <img src={SuccessImg} alt="success" />
+          <h1>Payment Successful</h1>
+          <p>
+            You have now registered for Turnah's concert. Use the payment
+            receipt sent to your email as your enterance permit.
+          </p>
+        </>
+      ) : (
+        <>
+          <img src={FailImg} alt="fail" />
+          <h1>Payment Not Successful</h1>
+          <p>Sorry, your payment wasn't accepted, please try again.</p>
+        </>
+      )}
+      <button onClick={handleClose}>Close</button>
+    </div>
+  );
 
   return (
-    <div className="concert">
-      <div className="main">
-        <div className="card">
-          <div className="show">
-            <h1>LifeStyle Party</h1>
-            <p>Book your ticket with ease</p>
-          </div>
+    <>
+      {showStatus && <PaymentStatus details={paymentDetails} />}
+      <div
+        className={`concert ${showStatus ? "site-dark" : undefined}`}
+        onClick={handleClose}
+      >
+        <div className="main">
+          <div className="card">
+            <div className="show">
+              <h1>LifeStyle Party</h1>
+              <p>Book your ticket with ease</p>
+            </div>
 
-          <div className="showcase">
-            <form onSubmit={handleSubmit}>
-              <div className="group">
-                <label for="fullName">Full Name</label>
-                <input
-                  type="text"
-                  value={fullName}
-                  id="fullName"
-                  name="fullName"
-                  placeholder="Enter full Name"
-                  required="true"
-                  onChange={handleInput}
-                />
-              </div>
-
-              <div className="group">
-                <label for="email">Email</label>
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  name="email"
-                  placeholder="Enter email"
-                  required="true"
-                  onChange={handleInput}
-                />
-              </div>
-
-              <div className="group">
-                <label for="type">Payment Type </label>
-                <div className="payment-type">
-                  {paymentOption.map((item, k) => (
-                    <button
-                      type="button"
-                      className={price === item.price && "active"}
-                      onClick={() => {
-                        setPrice(item.price)
-                        setIsPaymentSelected(false)
-                      }}
-                      key={k}
-                    >
-                      {item.type}
-                    </button>
-                  ))}
+            <div className="showcase">
+              <form onSubmit={handleSubmit}>
+                <div className="group">
+                  <label htmlFor="fullName">Full Name</label>
+                  <input
+                    type="text"
+                    value={fullName}
+                    id="fullName"
+                    name="fullName"
+                    placeholder="Enter full Name"
+                    required={true}
+                    onChange={handleInput}
+                  />
                 </div>
-                {isPaymentSelected && <p className="error">Please select a payment type</p>}
-              </div>
-              <button type="submit">
-                <p>Click here to book</p>
-              </button>
-            </form>
+
+                <div className="group">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    name="email"
+                    placeholder="Enter email"
+                    required={true}
+                    onChange={handleInput}
+                  />
+                </div>
+
+                <div className="group">
+                  <label htmlFor="type">Payment Type </label>
+                  <div className="payment-type">
+                    {paymentOption.map((item, k) => (
+                      <button
+                        type="button"
+                        className={price === item.price ? "active" : undefined}
+                        onClick={() => {
+                          setPrice(item.price);
+                          setIsPaymentSelected(false);
+                        }}
+                        key={k}
+                      >
+                        {item.type}
+                      </button>
+                    ))}
+                  </div>
+                  {isPaymentSelected && (
+                    <p className="error">Please select a payment type</p>
+                  )}
+                </div>
+                <button type="submit">
+                  <p>Click here to book</p>
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
